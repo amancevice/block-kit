f87bfd1e394595873b452d6cf1b2e34591812b25
Alexander Mancevice <alexander.mancevice@hey.com>
2022-04-16 10:20:07 -0400

    customize menu

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
-const { app, BrowserWindow } = require("electron");
+const { app, shell, BrowserWindow, Menu } = require("electron");
 const Store = require("electron-store");
 
 const store = new Store();
 const defaultUrl = new URL("https://app.slack.com/block-kit-builder/");
+const isMac = process.platform === "darwin";
 
 let win;
 
@@ -44,11 +45,101 @@ const appOnReady = () => {
   win.once("ready-to-show", win.show);
   win.webContents.on("did-finish-load", winWebContentsOnDidFinishLoad);
   if (store.get("openDevTools")) win.webContents.openDevTools();
+  const menuTemplate = [
+    ...(isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              { role: "about" },
+              { type: "separator" },
+              { role: "hide" },
+              { role: "hideOthers" },
+              { role: "unhide" },
+              { type: "separator" },
+              { role: "quit" },
+            ],
+          },
+        ]
+      : []),
+    {
+      label: "File",
+      submenu: [isMac ? { role: "close" } : { role: "quit" }],
+    },
+    {
+      label: "Edit",
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        { role: "delete" },
+      ],
+    },
+    {
+      label: "View",
+      submenu: [
+        { role: "reload" },
+        { role: "forceReload" },
+        { role: "toggleDevTools" },
+        { type: "separator" },
+        { role: "resetZoom" },
+        { role: "zoomIn" },
+        { role: "zoomOut" },
+        { type: "separator" },
+        {
+          label: "Toggle Dark Mode",
+          type: "checkbox",
+          accelerator: isMac ? "Cmd+Shift+D" : "Ctrl+Shift+D",
+          checked: store.get("theme") === "dark",
+          click: toggleTheme,
+        },
+        { type: "separator" },
+        {
+          role: "togglefullscreen",
+          accelerator: isMac ? "Cmd+Ctrl+F" : "F11",
+        },
+      ],
+    },
+    {
+      label: "Window",
+      submenu: [
+        { role: "minimize" },
+        { role: "zoom" },
+        ...(isMac ? [] : [{ role: "close" }]),
+      ],
+    },
+    {
+      role: "Help",
+      submenu: [
+        {
+          label: "Block Element Reference",
+          click: async () => {
+            await shell.openExternal(
+              "https://api.slack.com/reference/block-kit/block-elements"
+            );
+          },
+        },
+        {
+          label: "Issues",
+          click: async () => {
+            await shell.openExternal(
+              "https://github.com/amancevice/block-kit/issues"
+            );
+          },
+        },
+      ],
+    },
+  ];
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 };
 
 // All Windows Closed
 const appOnWindowAllClosed = () => {
-  app.quit();
+  if (isMac) app.quit();
 };
 
 // Window Closed
@@ -83,29 +174,45 @@ const winWebContentsOnDidFinishLoad = () => {
       max-height: calc(100vh - 118px) !important;
     }
   `;
-  var customJS = `
-    ${setDarkMode.toString()}
-    setDarkMode().catch(console.error);
-  `;
   win.webContents.insertCSS(customCSS);
+  if (store.get("theme") === "dark") enableDarkMode();
+};
+
+const toggleTheme = async () => {
+  store.get("theme") === "dark" ? enableLightMode() : enableDarkMode();
+};
+
+const enableDarkMode = () => {
+  store.set("theme", "dark");
+  var customJS = `${setTheme.toString()}\nsetTheme("dark");`;
+  win.webContents.executeJavaScript(customJS).catch(console.error);
+};
+
+const enableLightMode = () => {
+  store.set("theme", "light");
+  var customJS = `${setTheme.toString()}\nsetTheme("light");`;
   win.webContents.executeJavaScript(customJS).catch(console.error);
 };
 
 // Dark Mode
-function setDarkMode() {
-  return new Promise((resolve) => {
-    var menuId = "block-kit-builder-theme-select_button-option";
-    var menu = document.getElementById(menuId);
-    if (menu !== null) {
-      menu.click();
-      var opt1Id = "block-kit-builder-theme-select_option_1";
-      var opt1 = document.getElementById(opt1Id);
-      opt1.click();
-      resolve();
-    } else {
-      setTimeout(setDarkMode, 100);
-    }
-  });
+function setTheme(name) {
+  var menuId = "block-kit-builder-theme-select_button-option";
+  var optId = `block-kit-builder-theme-select_option_${
+    name === "dark" ? 1 : 0
+  }`;
+  function asyncSetTheme() {
+    return new Promise((resolve) => {
+      var menu = document.getElementById(menuId);
+      if (menu !== null) {
+        menu.click();
+        document.getElementById(optId).click();
+        resolve();
+      } else {
+        setTimeout(asyncSetTheme, 100);
+      }
+    });
+  }
+  return asyncSetTheme();
 }
 
 // Register App events
